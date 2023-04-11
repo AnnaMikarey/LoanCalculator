e89bd362e92b4c11f2f1d88db986945e85533c12
Anna <annaanisimovaal@gmail.com>
2023-04-11 17:25:11 +0300

    first commit

---
+++ server/src/test/java/com/server/loanCalculator/LoanCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.loanCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
