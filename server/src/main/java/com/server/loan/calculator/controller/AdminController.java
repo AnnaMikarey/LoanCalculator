a61dc2575b962ae36af5cd959c6230f19bd0752d
AudriusAnusauskas <aaaudrius@gmail.com>
2023-04-21 12:19:22 +0300

    merge with java

--- server/src/main/java/com/server/loan/calculator/controller/AdminController.java
+++ server/src/main/java/com/server/loan/calculator/controller/AdminController.java
@@ -3,19 +3,39 @@ package com.server.loan.calculator.controller;
 import com.server.loan.calculator.model.AdminData;
 import com.server.loan.calculator.service.AdminService;
 import lombok.RequiredArgsConstructor;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+=======
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200/")
+>>>>>>> f8de320 (merge with java)
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
 
+<<<<<<< HEAD
     @GetMapping("/get-values")
     public AdminData returnAllValues () {
 
         return adminService.returnAllValues();
+=======
+    @PostMapping(value = "/values", consumes = {"application/json"})
+    public  void saveToDatabase (@RequestBody AdminData data) {
+        adminService.addToDatabase(data);
+            }
+
+    @GetMapping("/values")
+    public ResponseEntity<AdminData> returnInitialValues () {
+        return new ResponseEntity<>(adminService.fetchFromDatabase(), HttpStatus.OK);
+>>>>>>> f8de320 (merge with java)
     }
 }
--- server/src/main/java/com/server/loan/calculator/controller/CalculatorController.java
+++ server/src/main/java/com/server/loan/calculator/controller/CalculatorController.java
@@ -1,5 +1,6 @@
 package com.server.loan.calculator.controller;
 
+<<<<<<< HEAD
 import com.server.loan.calculator.service.CalculatorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,4 +19,32 @@ public class CalculatorController {
         return calculator.returnInfo();
     }
 
+=======
+import com.server.loan.calculator.model.AdminData;
+import com.server.loan.calculator.model.CalculatorData;
+import com.server.loan.calculator.model.ResultsData;
+import com.server.loan.calculator.service.CalculatorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200/")
+@RequestMapping("/client")
+@RequiredArgsConstructor
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    @PostMapping(value = "/calculate-mortgage", consumes = {"application/json"})
+    public ResponseEntity<ResultsData> returnCalculatedResults (@RequestBody CalculatorData data) {
+
+        return new ResponseEntity<>(calculatorService.returnCalculatedData(data), HttpStatus.OK);
+    }
+
+    @PostMapping("/initial-values")
+    public ResponseEntity<AdminData> returnInitialValues () {
+        return new ResponseEntity<>(calculatorService.fetchValuesFromDatabase(), HttpStatus.OK);
+    }
+>>>>>>> f8de320 (merge with java)
 }
