580a709daea9757f165ec6761b63ff364e605916
Sandra P <prochorskaite@outlook.com>
2023-04-22 12:53:51 +0300

    only admin endpoints

--- server/src/main/java/com/server/loan/calculator/controller/CalculatorController.java
+++ server/src/main/java/com/server/loan/calculator/controller/CalculatorController.java
@@ -2,20 +2,22 @@ package com.server.loan.calculator.controller;
 
 import com.server.loan.calculator.service.CalculatorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/client")
+@CrossOrigin(origins = "http://localhost:4200/")
 @RequiredArgsConstructor
+
 public class CalculatorController {
     private final CalculatorService calculator;
 
-    @GetMapping("/calculate-mortgage")
+    @GetMapping("/values")
     public String returnCalculatedMortgage () {
 
         return calculator.returnInfo();
     }
-
 }
