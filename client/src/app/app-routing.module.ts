004edf68157cc4e9a671ddecd67a49c4cbc6c672
dziugassupinis <127023713+dziugassupinis@users.noreply.github.com>
2023-04-26 18:29:49 +0300

    added routing for user

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { UserParentComponent } from './components/user-parent/user-parent.component';
 
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminDashboardComponent },
+  { path: 'user', component: UserParentComponent },
 ];
 
 @NgModule({
