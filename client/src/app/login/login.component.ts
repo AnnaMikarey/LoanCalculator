0317504743e5dedf8c5fa41a98e267cac82a7b59
Anna <annaanisimovaal@gmail.com>
2023-04-28 09:24:16 +0300

    logInUIFised

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -4,21 +4,25 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  isError: boolean;
 
   constructor(private router: Router) {}
 
   login(): void {
     const hardcodedUsername: string = 'admin987';
     const hardcodedPassword: string = 'password985';
+    let isError: boolean = false;
+
 
     if (this.username === hardcodedUsername && this.password === hardcodedPassword) {
       this.router.navigate(['/admin']);
     } else {
-      alert('Invalid login or password');
+      this.isError = true;    
     }
   }
 }
