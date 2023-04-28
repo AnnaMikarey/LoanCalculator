ceebea0c881349e925e4b2e007688d9990ec94c1
IgnasDavulis <ignas.davulis@bazaarvoice.com>
2023-04-28 04:59:41 +0300

    Pushing changes for the domain

--- client/src/app/services/admin.service.ts
+++ client/src/app/services/admin.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AdminData } from '../../Types/adminData';
+import { environment } from "../environment";
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +12,14 @@ export class AdminService {
 
   postData(formData: AdminData): Observable<AdminData> {
     return this.http.post<AdminData>(
-      'http://16.16.120.205:8080/admin/change-values',
+      environment.baseUrl + "admin/change-values",
       formData
     );
   }
 
   getData(): Observable<AdminData> {
     return this.http.get<AdminData>(
-      'http://16.16.120.205:8080/admin/get-initial-values'
+      environment.baseUrl + 'admin/get-initial-values'
     );
   }
 }
--- client/src/app/services/calculator.service.ts
+++ client/src/app/services/calculator.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { InitialData } from '../Types/InitialData';
 import { UserData } from '../Types/UserData';
 import { CalculatedValues } from '../Types/CalculatedValues';
+import {environment} from "../environment";
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 }
@@ -12,15 +13,15 @@ const httpOptions = {
 })
 export class CalculatorService {
 
-  private apiUrl = 'http://16.16.120.205:8080/client/';
+  // private apiUrl = 'http://16.16.120.205:8080/client/';
   //private apiUrl = 'http://localhost:3000/';
   constructor(private httpClient: HttpClient) { }
 
   getInitialData(): Observable<any[]> {
-    return this.httpClient.get<any[]>(this.apiUrl + 'get-initial-values');
+    return this.httpClient.get<any[]>(environment.baseUrl + 'get-initial-values');
   }
 
   postUserDataGetsCalc(userData: UserData): Observable<CalculatedValues> {
-    return this.httpClient.post<CalculatedValues>(this.apiUrl + 'calculate', userData, httpOptions)
+    return this.httpClient.post<CalculatedValues>(environment.baseUrl + 'calculate', userData, httpOptions)
   }
 }
